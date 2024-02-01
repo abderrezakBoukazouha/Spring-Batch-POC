59b34b56be571209ddaeb6d5ad4e2639abbc0d83
Ziko <abderrezak.bkz@gmail.com>
2024-02-01 16:07:35 +0100

    adding integration tests for batch processing

---
+++ src/main/java/org/example/entity/Line.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+
+    @Id
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
